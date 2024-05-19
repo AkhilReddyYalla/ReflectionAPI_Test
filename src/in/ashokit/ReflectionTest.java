713b4cde71e309e507045b9c729fd1e9eff1425d
AKHILREDDY <167970017+AkhilReddyYalla@users.noreply.github.com>
2024-05-20 00:10:05 +0530

    Update ReflectionTest.java

--- src/in/ashokit/ReflectionTest.java
+++ src/in/ashokit/ReflectionTest.java
@@ -6,8 +6,8 @@ public class ReflectionTest {
 	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		try {
 			Class<?> cls = Class.forName("in.ashokit.Welcome");
-			Field field = cls.getDeclaredField("message");
-			field.setAccessible(true);
+			Field field = cls.getDeclaredField("message"); //loading varible
+			field.setAccessible(true); //making variable accessible
 			Object object = field.get(new Welcome());
 			String result = (String)object;
 			System.out.println(result);
